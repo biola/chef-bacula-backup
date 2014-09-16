biola/chef-bacula-backup

commit d0771dbfa45cddf93cc4e62c1878116729acb112

add basic rhel5 client support

recipes/client.rb
@@ -3,6 +3,7 @@
 # Recipe:: client
 #
 # Copyright 2012, computerlyrik
+# Copyright 2014, Biola University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,47 +20,92 @@
 
 ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 
-package "bacula-client"
-service "bacula-fd" do
+case node['platform_family']
+when 'rhel'
+  remote_file "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['lzo_url'].split('/').last}" do
+    source node['bacula']['fd_packages']['lzo_url']
+    checksum node['bacula']['fd_packages']['lzo_checksum']
+  end
+  rpm_package "lzo-2.06" do
+    source "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['lzo_url'].split('/').last}"
+  end
+  
+  remote_file "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['libfastlz_url'].split('/').last}" do
+    source node['bacula']['fd_packages']['libfastlz_url']
+    checksum node['bacula']['fd_packages']['libfastlz_checksum']
+  end
+  rpm_package "libfastlz-0.1" do
+    source "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['libfastlz_url'].split('/').last}"
+  end
+  
+  remote_file "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['bareoscommon_url'].split('/').last}" do
+    source node['bacula']['fd_packages']['bareoscommon_url']
+    checksum node['bacula']['fd_packages']['bareoscommon_checksum']
+  end
+  rpm_package "bareos-common" do
+    source "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['bareoscommon_url'].split('/').last}"
+  end
+  
+  remote_file "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['bareosfd_url'].split('/').last}" do
+    source node['bacula']['fd_packages']['bareosfd_url']
+    checksum node['bacula']['fd_packages']['bareosfd_checksum']
+  end
+  rpm_package "bareos-filedaemon" do
+    source "#{Chef::Config[:file_cache_path]}/#{node['bacula']['fd_packages']['bareosfd_url'].split('/').last}"
+  end
+  
+  clientservice = 'bareos-fd'
+  templatefile = '/etc/bareos/bareos-fd.conf'
+else
+  package "bacula-client"
+  
+  clientservice = 'bacula-fd'
+  templatefile = '/etc/bareos/bareos-fd.conf'
+end
+
+service clientservice do
   supports :restart => true
 end
 
 node.set_unless['bacula']['fd']['password'] = secure_password
 node.set_unless['bacula']['fd']['password_monitor'] = secure_password
 
-template "/etc/bacula/bacula-fd.conf" do
+# FIXME delete the unneeded bareos-fd file
+template templatefile do
   group node['bacula']['group']
   mode 0640
-  notifies :restart, "service[bacula-fd]"
+  source 'bacula-fd.conf.erb'
+  notifies :restart, "service[#{clientservice}]"
 end
 
+# FIXME - commenting these out for now so they aren't deployed everywhere
 # include scripts for mysql backup
-if node['mysql'] && node['mysql']['server_root_password']
-
-  template '/usr/local/sbin/backupdbs' do
-    mode 0500
-    user node['bacula']['user']
-    group 'root'
-  end
-
-  directory '/var/local/mysqlbackups' do
-    user node['bacula']['user']
-    action :create
-    recursive true
-  end
-
-  template '/usr/local/sbin/restoredbs' do
-    mode 0500
-    user node['bacula']['user']
-    group 'root'
-  end
-  directory '/var/local/mysqlrestores' do
-    user node['bacula']['user']
-    action :create
-    recursive true
-  end
-
-end
+#if node['mysql'] && node['mysql']['server_root_password']
+#
+#  template '/usr/local/sbin/backupdbs' do
+#    mode 0500
+#    user node['bacula']['user']
+#    group 'root'
+#  end
+#
+#  directory '/var/local/mysqlbackups' do
+#    user node['bacula']['user']
+#    action :create
+#    recursive true
+#  end
+#
+#  template '/usr/local/sbin/restoredbs' do
+#    mode 0500
+#    user node['bacula']['user']
+#    group 'root'
+#  end
+#  directory '/var/local/mysqlrestores' do
+#    user node['bacula']['user']
+#    action :create
+#    recursive true
+#  end
+#
+#end
 
 # include scripts for ldap backup
 if node['openldap'] && node['openldap']['slapd_type'] == "master"

attributes/client.rb
@@ -1,5 +1,17 @@
 default['bacula']['fd']['address'] = node['ipaddress']
 
+# RHEL 5 clients will use these pre-compiled bareos packages
+if node['platform_family'] == 'rhel'
+  default['bacula']['fd_packages']['lzo_url'] = 'http://download.bareos.org/bareos/release/latest/RHEL_5/x86_64/lzo-2.06-1.1.x86_64.rpm'
+  default['bacula']['fd_packages']['lzo_checksum'] = '3bfd74fb282b8506fa6306c06d93eb506ebefca24c2b697d5e65439b04cca9cf'
+  default['bacula']['fd_packages']['libfastlz_url'] = 'http://download.bareos.org/bareos/release/latest/RHEL_5/x86_64/libfastlz-0.1-2.1.x86_64.rpm'
+  default['bacula']['fd_packages']['libfastlz_checksum'] = 'd861cb937ca50e02768b348c1174648bacea973969d6c41e721332a4cc6cfbcf'
+  default['bacula']['fd_packages']['bareoscommon_url'] = 'http://download.bareos.org/bareos/release/latest/RHEL_5/x86_64/bareos-common-13.2.2-7.1.el5.x86_64.rpm'
+  default['bacula']['fd_packages']['bareoscommon_checksum'] = '331b66468609d4b502d302dc579b31bf30d3948b78c0cba86d37bc599dbd3c31'
+  default['bacula']['fd_packages']['bareosfd_url'] = 'http://download.bareos.org/bareos/release/latest/RHEL_5/x86_64/bareos-filedaemon-13.2.2-7.1.el5.x86_64.rpm'
+  default['bacula']['fd_packages']['bareosfd_checksum'] = '0bb5347ca7670d5a5a75462b539f9419fc16e5e5ed13ab90b305538a8fca5cc8'
+end
+
 ## AUTODETECT BACKUPABLE DATA
 
 # ZARAFA

.kitchen.yml
@@ -10,6 +10,7 @@ platforms:
     box_url: https://opscode-vm-bento.s3.amazonaws.com/vagrant/opscode_ubuntu-12.04_provisionerless.box
   run_list:
   - recipe[apt]
+- name: centos-5.10
 
 suites:
 - name: server_with_storage

attributes/default.rb
@@ -1,5 +1,10 @@
-default['bacula']['user'] = "bacula"
-default['bacula']['group'] = "bacula"
+unless node['platform_family'] == 'rhel'
+  default['bacula']['user'] = "bacula"
+  default['bacula']['group'] = "bacula"
+else
+  default['bacula']['user'] = "bareos"
+  default['bacula']['group'] = "bareos"
+end
 
 default['bacula']['messages']['mail'] = "bacula@#{node['domain']}"
 default['bacula']['messages']['operator'] = node['bacula']['messages']['mail']

metadata.rb
@@ -6,7 +6,7 @@ description      "Installs and autoconfigures bacula backup system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.3.0"
 
-%w{ ubuntu debian}.each do |os|
+%w{ ubuntu debian redhat }.each do |os|
   supports os
 end
 

templates/default/bacula-fd.conf.erb
@@ -30,10 +30,11 @@ Director {
 #
 FileDaemon {                          # this is me
   Name = <%= node['fqdn'] %>
-  FDport = 9102                  # where we listen for the director
+  FDport = 9102                  # where we listen for the director<% unless node['platform_family'] == 'rhel' %>
   WorkingDirectory = /var/lib/bacula
   Pid Directory = /var/run/bacula
-  Maximum Concurrent Jobs = 20
+  Maximum Concurrent Jobs = 20<% else %>
+  compatible = yes<% end %>
   FDAddress = <%= node['bacula']['fd']['address'] %>
 }
 

commit 14336fce83ac3391687449e4e612fd7358f475ed

fix ubuntu client error introduced in d0771dbf

recipes/client.rb
@@ -60,7 +60,7 @@ else
   package "bacula-client"
   
   clientservice = 'bacula-fd'
-  templatefile = '/etc/bareos/bareos-fd.conf'
+  templatefile = '/etc/bacula/bareos-fd.conf'
 end
 
 service clientservice do