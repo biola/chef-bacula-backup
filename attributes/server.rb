0f310c9c01ae47f4f071cfb82aa6520621b3de81
troyready <troy@troyready.com>
2014-09-18 16:06:03 -0700

    Add optional override of default job pool/storage

--- attributes/server.rb
+++ attributes/server.rb
@@ -7,3 +7,6 @@ default['bacula']['volume_max'] = 50
 default['bacula']['label_format'] = "BaculaFile"
 
 default['bacula']['dir']['max_concurrent_jobs'] = "1"
+
+default['bacula']['dir']['file_storage_device'] = "File"
+default['bacula']['dir']['file_storage_pool'] = "File"
