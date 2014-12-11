c5fb319415142c5c7c7cfac3a562b8a82cfa4fff
troyready <troy@troyready.com>
2014-12-11 10:32:17 -0800

    update metadata
    https://github.com/opscode/chef/blob/12-stable/RELEASE_NOTES.md#new-cookbook-metadata-attributes-for-supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,18 @@
-name             "bacula-backup"
-maintainer       "Biola University"
-maintainer_email "troy.ready@biola.edu"
-license          "Apache 2.0"
-description      "Installs and autoconfigures bacula backup system"
+name             'bacula-backup'
+maintainer       'Biola University'
+maintainer_email 'troy.ready@biola.edu'
+license          'Apache 2.0'
+description      'Installs and autoconfigures bacula backup system'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.7.3"
+source_url       'https://github.com/biola/chef-bacula-backup'
+issues_url       'https://github.com/biola/chef-bacula-backup/issues'
+version          '1.7.3'
 
 %w{ ubuntu debian redhat windows }.each do |os|
   supports os
 end
 
-depends        "mysql", "~> 5.5"
+depends        'mysql', '~> 5.5'
 
 %w{ openssl database windows }.each do |dep|
   depends dep
